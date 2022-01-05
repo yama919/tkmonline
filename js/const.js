a8f6644a071f91a20b0be61ceffff74b7cac4b94
おまつ先生 <33661309+omatztw@users.noreply.github.com>
2022-01-06 00:54:00 +0900

    Change server URL

--- js/const.js
+++ js/const.js
@@ -1,3 +1,4 @@
-WSURL = 'wss://catasorasp.omatz.cf';
+// WSURL = 'wss://catasorasp.omatz.cf';
 // WSURL = 'wss://cataso2.omatz.cf';
+WSURL = 'wss://cataso-omatz.herokuapp.com';
 // WSURL = 'ws://localhost:5000';
